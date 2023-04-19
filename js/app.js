5e79e4c1cc3df353e34e7ceca7e412cc3a32caec
franciscorodriguezsv24 <103607889+franciscorodriguezsv24@users.noreply.github.com>
2023-04-19 07:46:55 -0600

    Final details

--- js/app.js
+++ js/app.js
@@ -42,76 +42,83 @@ findList.forEach( elemt => {
     })
 })
 
-//Creando filtro del botton 
+//filtro de ambos.
+const buttonSearch3 = dom.$('#btSearch');
 
-findList.forEach(elem => {
-    elem.addEventListener('click', (elemt) =>{
-        const buttonSearch = dom.$('#btSearch');
-
-
-const datacity = elemt.srcElement.childNodes[1].data;
-
-console.log(datacity)
-
-    buttonSearch.addEventListener('click', () => {
-       
-    let filter = datacity;
-
-    dom.stayPlaces(filter)
-  
-    const finishFiltered = filter === '' ? showData : data.cityFilter(showData,filter);
-   
-    dom.filterCard(finishFiltered)
-
-    const locationNumber = finishFiltered.length
-    
-    dom.stayNumber(locationNumber);
-    console.log (locationNumber)
-
-   
-        })
-
-    })
-
-})
-
-
-
-
-
-const buttonSearch = dom.$('#btSearch');
-
-    buttonSearch.addEventListener('click', () => {
+    buttonSearch3.addEventListener('click', () => {
+        //city
+        const datacity = dom.$('.modal').children;
+        const citi = datacity[0].childNodes[1].childNodes[3].childNodes[1].childNodes[3].childNodes[0].data;
+    console.log(citi)
+        //Number
         const dataGuestChild = [...dom.$('.modal').children]  
-    let finaldatachild = dataGuestChild[0].childNodes[1].childNodes[3].childNodes[9].childNodes[1].childNodes[3].childNodes[7].childNodes[0].data
-    let numberDataChild = Number(finaldatachild);
-
+        let finaldatachild = dataGuestChild[0].childNodes[1].childNodes[3].childNodes[9].childNodes[1].childNodes[3].childNodes[7].childNodes[0].data
+        let numberDataChild2 = Number(finaldatachild);
     
-
-    const dataGuest = [...dom.$('.modal').children] 
-    let finaldata = dataGuest[0].childNodes[1].childNodes[3].childNodes[9].childNodes[1].childNodes[1].childNodes[7].childNodes[0].data
-    let numberData = Number(finaldata)
-
-   let suma = numberData + numberDataChild
+        
     
-   const maxGuest = suma == 0 ? showData : data.guestFilter(showData, suma)
-   
-   dom.filterCard(maxGuest);
+        const dataGuest = [...dom.$('.modal').children] 
+        let finaldata = dataGuest[0].childNodes[1].childNodes[3].childNodes[9].childNodes[1].childNodes[1].childNodes[7].childNodes[0].data
+        let numberData2 = Number(finaldata)
+    
+        let suma2 = numberData2 + numberDataChild2
 
-   const dataMaxGuests = maxGuest.length
-  
-   dom.stayNumber(dataMaxGuests);
- 
 
+        
+            let finishFiltered = citi.innerHTML !== citi && suma2 == 0 ? data.cityFilter(showData,citi):showData
+        console.log(citi)
+            dom.stayPlaces(citi)
+
+        dom.filterCard(finishFiltered)
+
+        const locationNumber = finishFiltered.length
+    
+        dom.stayNumber(locationNumber);
+    }
+    )
+
+
+
+
+    // filtro Max Guests
+    const buttonSearch1 = dom.$('#btSearch');
+    
+        buttonSearch1.addEventListener('click', () => {
+            const dataGuestChild = [...dom.$('.modal').children]  
+        let finaldatachild = dataGuestChild[0].childNodes[1].childNodes[3].childNodes[9].childNodes[1].childNodes[3].childNodes[7].childNodes[0].data
+        let numberDataChild = Number(finaldatachild);
+    
+        const dataGuest = [...dom.$('.modal').children] 
+        let finaldata = dataGuest[0].childNodes[1].childNodes[3].childNodes[9].childNodes[1].childNodes[1].childNodes[7].childNodes[0].data
+        const numberData = Number(finaldata)
+
+        const datacity = dom.$('.modal').children;
+        const citi = datacity[0].childNodes[1].childNodes[3].childNodes[1].childNodes[3].childNodes[0].data;
+    
+       const suma = numberData + numberDataChild
+
+        if( suma !== 0 && citi.textContent !== citi){
+            const maxGuest = data.guestFilter(showData, suma);
+
+            console.log(maxGuest)
+
+            dom.filterCard(maxGuest)
+
+            const dataFinal = maxGuest.length
+      
+            dom.stayNumber(dataFinal);
+        }else if(suma !== 0 && citi.innerHTML == citi){
+       
+         const supremeFilter = data.supremeFilter(showData,citi,suma)
+         console.log(supremeFilter);
+
+
+         dom.filterCard(supremeFilter);
+
+         const dataMaxGuests = supremeFilter.length;
+
+         dom.stayNumber(dataMaxGuests);
+        }
     })
-
-
-
-   
-
-
-
-
-
-
-
+      
+       
